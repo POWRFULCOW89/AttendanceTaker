cc834c5bac5989c74ce1e7fc833adf7b1cbd952c
POWRFULCOW89 <57471984+POWRFULCOW89@users.noreply.github.com>
2021-04-12 15:43:03 -0500

    Added error handling

--- attendance.py
+++ attendance.py
@@ -1,11 +1,31 @@
 import os, sys, json
 import selenium as se
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 # Helper functions
 def pplink(ls): # "Pretty print link" Displays the text properties of a list. Debug
     print(list(map(lambda x: x.text, ls)))
 
+class CredentialsError(Exception):
+    def __init__(self, msg="Credentials are invalid."):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class MoodleError(Exception):
+    def __init__(self, msg="Moodle is currently down or the URL is not valid."):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class TimeoutError(Exception):
+    def __init__(self, msg="Webdriver timed out. Is the server up?"):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class AttendanceError(Exception):
+    def __init__(self, msg="The attendance page does not exist or attendance is not being taken."):
+        self.msg = msg
+        super().__init__(self.msg)
 
 # There should be only one instance of a webdriver
 class Singleton (type):
@@ -23,58 +43,100 @@ class AttendanceTaker(metaclass=Singleton):
         self.credentials = {}
 
     def currentPage(self):
-        return self.browser.title
+        return "Current page: " + self.browser.title + " at: " + self.browser.current_url
+
+    def isURLValid(self):
+        if self.currentPage() == "Error":
+            raise MoodleError
 
     def loadCredentials(self, file):
         with open(file) as json_file:
             self.credentials = json.load(json_file)
 
     def login(self):
-        # Getting there
-        self.browser.get("{0}/login/index.php".format(self.base_url))
+        starterURL = "{0}/login/index.php".format(self.base_url)
+        try:
+            # Getting there
+            self.browser.get(starterURL)
 
-        # Inputting credentials
-        username = self.browser.find_element_by_id("username")
-        username.send_keys(self.credentials["username"])
-        password = self.browser.find_element_by_id("password")
-        password.send_keys(self.credentials["password"])
+            # and assuring it
+            self.isURLValid()
+            print(self.currentPage())
 
-        # Submitting credentials
-        self.browser.find_element_by_id("loginbtn").submit()
+            # Inputting credentials
+            username = self.browser.find_element_by_id("username")
+            username.send_keys(self.credentials["username"])
+            password = self.browser.find_element_by_id("password")
+            password.send_keys(self.credentials["password"])
+
+            # Submitting credentials
+            self.browser.find_element_by_id("loginbtn").submit()
+
+        except NoSuchElementException:
+            raise MoodleError
+
+        except TimeoutException:
+            raise TimeoutError
+
+        if starterURL == self.browser.current_url:
+            raise CredentialsError
+
+        self.isURLValid()
 
     def navigateToCourseById(self, courseId):
         # Locate course. 
         self.browser.get("{0}/course/view.php?id={1}".format(self.base_url, courseId))
+        self.isURLValid()
 
-    def navigateToAttendanceById(self, attendanceListId):
+    def navigateToAttendanceById(self, attendanceListId): # Currently unused.
         # Locate attendance list. 
         self.browser.get("{0}/mod/attendance/view.php?id={1}".format(self.base_url, attendanceListId))
+        self.isURLValid()
 
     def takeAttendance(self): # Assuming the webdriver is currently on a course page.
-        # Grab all links that lead to attendance lists.
-        attendanceLinks = self.browser.find_elements_by_class_name("aalink")
-        for link in attendanceLinks[:]:
-            href = link.get_attribute("href")
-            if "attendance" not in href:
-                attendanceLinks.remove(link)
+        try:
+            # Grab all links that lead to attendance lists.
+            attendanceLinks = self.browser.find_elements_by_class_name("aalink")
 
-        # Submit attendance     (CURRENTLY UNTESTED)
-        for link in attendanceLinks:
-            # Navigating to the attendance page
-            self.browser.get(link.get_attribute("href"))
-            # Getting all possible links                 
-            attendanceBtns = self.browser.find_elements_by_class_name("form-check-input")
-            for btn in attendanceBtns:
-                btn.click()     # Might lead to unstable behavior
+            if len(attendanceLinks) == 0:
+                raise AttendanceError
 
-            # Clicking ok. We don't need a ref.
-            self.browser.find_element_by_name("submitbutton").click()
+            for link in attendanceLinks[:]:
+                href = link.get_attribute("href")
+                if "attendance" not in href:
+                    attendanceLinks.remove(link)
+
+            # Submit attendance     (CURRENTLY UNTESTED)
+            for link in attendanceLinks:
+                # Navigating to the attendance page
+                self.browser.get(link.get_attribute("href"))
+                
+                self.isURLValid()
+
+                print(self.currentPage())
+
+                # Getting all possible links                 
+                attendanceBtns = self.browser.find_elements_by_class_name("form-check-input")
+                for btn in attendanceBtns:
+                    btn.click()     # Might lead to unstable behavior
+
+                # Clicking ok. We don't need a ref.
+                self.browser.find_element_by_name("submitbutton").click()
+
+        except NoSuchElementException: #TODO: Remove double warning
+            raise 
 
     def logout(self):
-        # Click the user panel.
-        self.browser.find_element_by_id("action-menu-toggle-1").click()
-        # Click the logout button. Can't click if hidden.
-        self.browser.find_element_by_xpath("//a[@data-title='logout,moodle']").click()
+        try:
+            # Click the user panel.
+            self.browser.find_element_by_id("action-menu-toggle-1").click()
+            # Click the logout button. Can't click if the panel was hidden. TODO: Is there an URL for logging out?
+            self.browser.find_element_by_xpath("//a[@data-title='logout,moodle']").click()
+            print(self.currentPage())
+
+        except NoSuchElementException:
+            raise MoodleError
 
     def exit(self):
         self.browser.quit()
+        
--- example.py
+++ example.py
@@ -2,27 +2,28 @@ from attendance import AttendanceTaker
 
 # Constraints
 
-base_url = "https://avalid.moodleplatform.com.uk"
+base_url = "https://avalid.moodleplatform.com.uk/course/view.php?id=1111"
 AT = AttendanceTaker(base_url)
 
 ##############################################
 
-# Login
-AT.loadCredentials("credentials.json")
-AT.login()
+try:
+    # Login
+    AT.loadCredentials("credentials.json")
+    AT.login()
 
-# Look up course
-print(AT.currentPage())
-AT.navigateToCourseById(2481) # Embedded in the url: "https://avalid.moodleplatform.com.uk/course/view.php?id=1111"
+    # Look up course
+    AT.navigateToCourseById(2481) # Embedded in the url: "https://avalid.moodleplatform.com.uk/course/view.php?id=1111"
 
-# Take attendance
-print(AT.currentPage())
-AT.takeAttendance()        
+    # Take attendance
+    AT.takeAttendance()        
 
-# Logout
-AT.logout()
+    print("Attendance has been taken. Please verify.")
 
-print(AT.currentPage())
-AT.exit()
+    # Logout
+    AT.logout()
 
-print("Attendance has been taken. Please verify.")
+finally:
+    # Clean up afterwards
+    AT.exit()
+    
